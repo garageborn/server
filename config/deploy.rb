baa53fbb09ef17e3ba8d4355d514780b373b819c
Alexandre Bini <alexandrebini@gmail.com>
2017-02-01 00:18:17 -0200

    update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '3.7.1'
+lock '3.7.2'
 
 set :application, 'server'
 set :repo_url, 'git@github.com:garageborn/server.git'
@@ -6,7 +6,7 @@ set :keep_releases, 10
 set :deploy_to, '/home/garageborn/server'
 set :pty, true
 set :root, File.expand_path(File.dirname(__FILE__) + '/../')
-set :ssh_options, { forward_agent: true, port: 41858 }
+set :ssh_options, forward_agent: true, port: 41_858
 set :branch, -> { ENV['branch'] || `git rev-parse --abbrev-ref HEAD`.chomp }
 set :use_sudo, false
 
@@ -18,7 +18,6 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 
 # slackistrano
-set :slackistrano, {
+set :slackistrano,
   channel: '#activities',
   webhook: 'https://hooks.slack.com/services/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'
-}
