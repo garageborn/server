d76e91136b3642fc961028bb5f59c183ce1befc2
Alexandre Bini <alexandrebini@gmail.com>
2017-06-08 12:56:57 -0300

    logrotate maxage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'app01.server.garageborn.com', user: 'garageborn', roles: %w{app ssl}
-server 'app02.server.garageborn.com', user: 'garageborn', roles: %w{app}
-server 'app03.server.garageborn.com', user: 'garageborn', roles: %w{app}
-server 'worker01.server.garageborn.com', user: 'garageborn', roles: %w{db worker}
+server 'app01.server', user: 'garageborn', roles: %w{app ssl}
+server 'app02.server', user: 'garageborn', roles: %w{app}
+server 'app03.server', user: 'garageborn', roles: %w{app}
+server 'worker01.server', user: 'garageborn', roles: %w{db worker}
