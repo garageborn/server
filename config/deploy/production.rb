baedd9cc909a49f662a64e9c0f14a0499f6aa1b9
Alexandre Bini <alexandrebini@gmail.com>
2017-06-12 11:33:41 -0300

    Create production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
 server 'app01.server', user: 'garageborn', roles: %w{app ssl}
 server 'app02.server', user: 'garageborn', roles: %w{app}
 server 'app03.server', user: 'garageborn', roles: %w{app db worker}
-# server 'worker01.server', user: 'garageborn', roles: %w{db worker}
